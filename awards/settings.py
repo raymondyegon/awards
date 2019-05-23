236f7614ae2347375c01623b228326cffb2f2716
Raymond Yegon <raymondyegon17@gmail.com>
2019-05-23 18:45:06 +0300

    added bootstrap, and gift app to installed apps

--- awards/settings.py
+++ awards/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap4',
+    'imagekit',
+    'django_registration',
+    'gift',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
