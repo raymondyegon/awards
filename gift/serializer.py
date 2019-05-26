1a14617dc7fd9a560b4b034585fb1b3658fb02af
Raymond Yegon <raymondyegon17@gmail.com>
2019-05-26 19:13:31 +0300

    setup the profile and project serializer

---
+++ gift/serializer.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Profile, Project
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ('user', 'rate')
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('user', 'profile')
